78080d2be8b50793efd019edd9e89d2a6d78fce1
clement Roland <clementroland52@gmail.com>
2026-01-19 14:28:32 -0400

    Enhance App component to connect to API and log data; add .env to .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { useState,useEffect } from "react"
 
 function App() {
-  
+  // 🔑 take the URL of Api in .env 
+  const API = import.meta.env.VITE_URL_API
+  useEffect(()=>{
+    ConnectApi()
+    console.log('connexion Api:',API)
+  },[])
+// fonction for the connect my project with API 
+  const ConnectApi = () =>{
+     fetch(API)
+          .then(reponse => reponse.json())
+          .then(data => console.log(data))
+          .catch(error => console.log(error))
+  }
+
+
 }
 
 export default App
